9d1f88cd1c2d79eeb77e08843275521a7310b077
Sujeong Jo <bestsjj@naver.com>
2021-10-05 23:47:55 +0900

    백준단계별

---
+++ 백준/N14681.java
@@ -0,0 +1,26 @@
+// 백준 if문 단계별
+
+import java.util.Scanner;
+
+public class N14681 {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		sc.close();
+		
+		if( x > 0 && y > 0) {
+			System.out.println("1");
+		}else if(x < 0 && y > 0) {
+			System.out.println("2");
+		}else if(x < 0 && y < 0) {
+			System.out.println("3");
+		}else {
+			System.out.println("4");
+		}
+	}
+
+}
+
+
