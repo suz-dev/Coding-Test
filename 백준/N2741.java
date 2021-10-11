31abf50aa767a5edfed781556daf945509a160ff
Sujeong Jo <bestsjj@naver.com>
2021-10-11 23:23:58 +0900

    BR ver

--- 백준/N2741.java
+++ 백준/N2741.java
@@ -1,20 +1,23 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * 자연수 'N'이 주어졌을 때, 1부터 'N'까지 한 줄에 하나씩 출력하는 프로그램을 작성하시오. 
- * 'for'문 사용
+ * 'BufferedReader' 사용
  */
 public class N2741 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
-		Scanner scanner = new Scanner(System.in);
-		int N = scanner.nextInt();
-		scanner.close();
-
-		for (int i = 1; i <= N; i++) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		br.close();
+		
+		for(int i = 1; i <= N; i++) {
 			System.out.println(i);
 		}
+		
 	}
 
 }
