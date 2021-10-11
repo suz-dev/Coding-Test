0b7807a73a3c14546c1851050b9ce3a71890b582
Sujeong Jo <bestsjj@naver.com>
2021-10-11 23:59:55 +0900

    example

--- 백준/N15552.java
+++ 백준/N15552.java
@@ -6,11 +6,11 @@ import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class N15552 {
-
+	
 	/**
-	 * 'for'문 단계별 - 빠른 'A+B' 'Scanner'대신 'BufferedReader, BufferedWriter' 사용
-	 * 'BufferedReader' 변수 설정, 'input'값은 모두 'BufferedReader.readLine()'으로 받음
-	 * 'BufferedWriter' 변수 설정, 'output'값은 모두 'BufferedWriter.write()'으로 받음
+	 * 'for'문 빠른 'A+B' 추가버전 (1552->11021->11022)
+	 * 각 테스트 케이스마다 "Case #x: A + B = C" 형식으로 출력한다. 
+	 * x는 테스트 케이스 번호이고 1부터 시작하며, 'C = A+B'이다.
 	 */
 
 	public static void main(String[] args) throws IOException {
@@ -18,25 +18,28 @@ public class N15552 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		/**
-		 * 정수형 변수 'T'를 설정하여 반복횟수 입력받음 
-		 * 테스트 케이스로 입력될 두 정수를 받을 'StringTokenizer'메소드 변수'st'추가 
-		 * 입력받은 'T'의 반복횟수만큼 반복('for'문) 'StringTokenizer'를 통해 두 정수 입력받음
-		 * 'BufferWirter.wirte()'에 두 정수의 합 저장
+		/** 
+		 * 정수형 변수 'a', 'b' 정의
 		 */
 		int T = Integer.parseInt(br.readLine());
-
-		StringTokenizer st;
+		int a;
+		int b;
+		
+		/**
+		 * 'StringTokenizer' 초기값 설정
+		 * 'st'를 통해 정수 'a', 'b' 입력받음
+		 */
+		StringTokenizer st = null;
 
 		for (int i = 0; i < T; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-			bw.write((Integer.parseInt(st.nextToken())) + Integer.parseInt(st.nextToken()) + "\n");
+			st = new StringTokenizer(br.readLine());
+			a = Integer.parseInt(st.nextToken());
+			b = Integer.parseInt(st.nextToken());
+
+			bw.write("Case #" + (i + 1) + ": " + a + " + " + b + " = " + (a + b));
+			bw.newLine();
 		}
 
-		/*
-		 * 'Scanner'와 마찬가지로 'BufferedReader'와 'BufferedWriter'도 'close()'를 실행해야 함
-		 * 'BufferedWriter'의 경우 'buffer flush()'를 통해 남아 있는 데이터를 출력시키고 'close()'해야 함 (버퍼 비우기)
-		 */
 		br.close();
 
 		bw.flush();
