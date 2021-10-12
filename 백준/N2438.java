7fe14a2d2484c51c4fd2c45940cdfead44a8cf47
Sujeong Jo <bestsjj@naver.com>
2021-10-12 23:26:42 +0900

    for문 단계별

---
+++ 백준/N2438.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+/**
+ * น้มุ 2438 -> 2439
+ */
+public class N2438 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		sc.close();
+
+		for (int i = 1; i <= N; i++) {
+
+			for (int j = 1; j <= N - i; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+
+			System.out.println();
+		}
+
+	}
+
+}
