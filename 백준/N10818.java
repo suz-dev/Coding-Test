782f2b16931b00c91d7b6970e0ace2067ca4f618
Sujeong Jo <bestsjj@naver.com>
2021-10-21 23:48:35 +0900

    단계별 배열2

--- 백준/N10818.java
+++ 백준/N10818.java
@@ -1,30 +1,41 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class N10818 {
 
 	/**
-	 * 'N'개의 정수가 주어질 때 배열을 이용하여 최댓값과 최솟값 구하기
+	 * 9개의 정수가 주어질 때 배열을 이용하여 최댓값 구하기 (10818 -> 2562)
 	 */
 
 	public static void main(String[] args) {
-		
+
 		/**
-		 * 'Scanner'를 이용하여 입력받을 정수의 개수 먼저 입력받음 (N)
-		 * 길이가 'N'인 배열 선언
-		 * 배열에 들어갈 'N'개의 정수를 입력받음
-		 * 'Arrays.sort()'메소드를 통해 최솟값과 최댓값 구하기
+		 * 'Scanner'를 이용하여 길이가 9인 배열을 입력받아 초기화한다
+		 *  최댓값을 입력받을 변수 'max'와 배열의 순서를 나타내줄 변수 'count', 'index'를 초기화한다
 		 */
 
 		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int[] arr = new int[N];
-
-		for (int i = 0; i < N; i++) {
-			arr[i] = sc.nextInt();
-		}
+		int[] arr = { sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt(),
+				sc.nextInt(), sc.nextInt(), };
 		sc.close();
-		Arrays.sort(arr);
-		System.out.println(arr[0] + " " + arr[N - 1]);
+
+		int max = 0;
+		int count = 0;		
+		int index = 0;
+		
+		/**
+		 * 'value'값이 'arr'배열값일 동안 반복한다
+		 * 'value'값이 'max'보다 크다면 'max=value', 'index=count'로 저장하고 
+		 * 첫째줄에 'max'값 출력, 둘째줄에 'index'값을 출력한다
+		 */
+
+		for (int value : arr) {
+			count++;
+
+			if (value > max) {
+				max = value;
+				index = count;
+			}
+		}
+		System.out.print(max + "\n" + index);
 	}
 }
