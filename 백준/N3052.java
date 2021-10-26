a42110e56c0c7290cf4e0b312655519550271fa2
Sujeong Jo <bestsjj@naver.com>
2021-10-26 23:42:12 +0900

    boolean array

--- 백준/N3052.java
+++ 백준/N3052.java
@@ -1,53 +1,37 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class N3052 {
-	
+
 	/**
 	 * 수 10개를 입력받은 뒤, 이를 42로 나눈 나머지를 구한 다음, 서로 다른 값이 몇 개 있는지 출력하는 프로그램을 작성하시오.
 	 */
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws IOException {
+
 		/**
-		 * 'Scanner'를 사용하여 수를 입력받아야 한다
-		 * 길이가 10인 'arr'배열을 선언한다
-		 * 나머지에 대한 중복값의 개수를 저장할 변수 'k'를 선언한다
-		 * 중복값이 없는 경우를 저장할 변수 'count'를 선언한다
+		 * 나머지의 값으로 나올 수 있는 0 ~ 41 까지를 담을 수 있는 'boolean'배열 'arr'를 생성한다('boolean'배열의 'default'값은 'false'이다) 
+		 * 'BufferedReader'를 통해 숫자를 입력받는다
+		 * 반복문을 실행하여 입력받은 값에 %42를 실행한 값의 'index'를 'true'로 바꿔준다 
+		 * 반복문 종료 후 개수를 저장할 변수 'count'를 선언한 후 다음 반복문을 실행하여 첫번째 반복문에서 'true'로 바꿔준 값에 대하여 'count++'를 실행한다 
+		 * 반복문 종료 후 'count'값을 출력한다
 		 */
 
-		Scanner sc = new Scanner(System.in);
-		int[] arr = new int[10];
-		
-		int k = 0;
-		int count = 0;
-		
-		/**
-		 * 반복문을 통해 10개의 숫자를 입력받아 42로 나눈 값을 'arr'배열에 저장한다
-		 * 
-		 * 다음 반복문을 통해 중복값이 존재하는지에 대한 검사를 실행한다
-		 * 이중'for'문을 통해 배열 'arr[i]'와 'arr[j]'를 순차적으로 비교하여 중복값을 찾는다
-		 * 중복값이 존재할 경우 'k++'를 실행한다
-		 * 
-		 * 반복문을 빠져나온 뒤 조건문을 통해 중복값이 없는 경우('k == 0'인 경우) 'count++'를 실행한다
-		 */
+		boolean[] arr = new boolean[42];
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		for (int i = 0; i < arr.length; i++) {
-			arr[i] = sc.nextInt() % 42;
+		for (int i = 0; i < 10; i++) {
+			arr[Integer.parseInt(br.readLine()) % 42] = true;
 		}
 
-		for (int i = 0; i < arr.length; i++) {
-			k = 0;
-			for (int j = i + 1; j < arr.length; j++) {
-				if (arr[i] == arr[j]) {
-					k++;
-				}
-			}
+		int count = 0;
 
-			if (k == 0) {
+		for (boolean value : arr) {
+			if (value) {
 				count++;
 			}
 		}
-		sc.close();
 		System.out.println(count);
 	}
 
