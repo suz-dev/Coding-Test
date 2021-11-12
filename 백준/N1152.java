d0a256aa0f43267e193ee80e7bab9e3cf8b5b3d8
Sujeong Jo <bestsjj@naver.com>
2021-11-12 16:56:13 +0900

    str 설명

--- 백준/N1152.java
+++ 백준/N1152.java
@@ -4,18 +4,18 @@ public class N1152 {
 
 	public static void main(String[] args) throws IOException {
 
-		int cnt = 0;
-		int pre_str = 32;
-		int str;
+		int cnt = 0; // 단어의 개수
+		int pre_str = 32; // 이전 단어
+		int str; // 입력받은 단어
 
 		while (true) {
 			str = System.in.read();
-
-			if (str == 32) {
+			
+			if (str == 32) { // 'str'이 공백일 때
 				if (pre_str != 32) {
 					cnt++;
 				}
-			} else if (str == 10) {
+			} else if (str == 10) { // 'str'이 개행일 때
 
 				if (pre_str != 32) {
 					cnt++;
@@ -23,7 +23,7 @@ public class N1152 {
 				break;
 
 			}
-			pre_str = str;
+			pre_str = str; // 'pre_str'를 'str'로 대치
 		}
 
 		System.out.println(cnt);
