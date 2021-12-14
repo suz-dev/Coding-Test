ca93a02a7de7ba2693d68bd88b439be3d7f12955
Sujeong Jo <bestsjj@naver.com>
2021-12-15 01:57:01 +0900

    array3

---
+++ LeetCode/SquaresOfASortedArray.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class SquaresOfASortedArray {
+	
+	// Time Complexity: O(N log N), Space Complexity: O(log N)
+
+	public int[] sortedSquares(int[] nums) {
+
+		int[] arr = new int[nums.length]; // nums배열의 요소들을 각각 제곱하여 저장할 배열 arr 선언
+
+		for (int i = 0; i < nums.length; i++) {
+			arr[i] *= arr[i];
+		}
+
+		Arrays.sort(arr); // 오름차순 정렬 메소드
+		return arr;
+	}
+}
