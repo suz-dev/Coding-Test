a52280b5a0f9df404f10dae90ff4c993fae2d541
Sujeong Jo <bestsjj@naver.com>
2021-12-14 16:05:13 +0900

    array discussion

--- LeetCode/MaxConsecutiveOnes.java
+++ LeetCode/MaxConsecutiveOnes.java
@@ -4,21 +4,18 @@ class Solution {
 	public static int MaxConsecutiveOnes(int[] nums) {
 
 		int cnt = 0;
-		int max = 0;
+		int result = 0;
 
 		for (int i = 0; i < nums.length; i++) {
 
 			if (nums[i] == 1) {
 				cnt++;
-				if (cnt > max) {
-					max = cnt;
-				}
+				result = Math.max(cnt, result); // if문 대신 Math.max()메소드 활용
 			} else {
 				cnt = 0;
 			}
 		}
-		return max;
+		return result;
 	}
 
 }
-
