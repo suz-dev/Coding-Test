476eae23588f1e202a66a309f20aa43997afcd1f
Sujeong Jo <bestsjj@naver.com>
2022-02-04 23:24:48 +0900

    Level 1

---
+++ 프로그래머스/Array/phoneNumber.java
@@ -0,0 +1,23 @@
+package Array;
+
+public class phoneNumber {
+
+	public String solution(String phone_number) {
+		String answer = "";
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < phone_number.length(); i++) {
+
+			if (i < phone_number.length() - 4) {
+				sb.append('*');
+			} else {
+				sb.append(phone_number.charAt(i));
+			}
+
+		}
+
+		answer = sb.toString();
+
+		return answer;
+	}
+}
