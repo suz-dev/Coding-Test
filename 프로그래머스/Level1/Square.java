9adc9e7402bb2cdfddaf6a50fd9244334434df99
Sujeong Jo <bestsjj@naver.com>
2022-02-06 19:23:25 +0900

    Level 1

---
+++ 프로그래머스/Level1/Square.java
@@ -0,0 +1,19 @@
+package Level1;
+
+public class Square {
+
+	public long solution(long n) {
+		long answer = 0;
+
+		for (int i = 0; i * i <= n; i++) {
+			if (i * i == n) {
+				answer = (i * 1) * (i * 1);
+			} else {
+				answer = -1;
+			}
+
+		}
+
+		return answer;
+	}
+}
