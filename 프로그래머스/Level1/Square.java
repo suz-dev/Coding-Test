4a467d245667221eaa1c4d3794d29959992408af
Sujeong Jo <bestsjj@naver.com>
2022-02-07 23:37:49 +0900

    Level 1

--- 프로그래머스/Level1/Square.java
+++ 프로그래머스/Level1/Square.java
@@ -1,5 +1,9 @@
 package Level1;
 
+/*
+ * n이 양의 정수 x의 제곱이라면 x+1의 제곱을 리턴하고, 
+ * n이 양의 정수 x의 제곱이 아니라면 -1을 리턴하는 함수를 완성하세요.
+ */
 public class Square {
 
 	public long solution(long n) {
@@ -7,7 +11,7 @@ public class Square {
 
 		for (int i = 0; i * i <= n; i++) {
 			if (i * i == n) {
-				answer = (i * 1) * (i * 1);
+				answer = (i + 1) * (i + 1);
 			} else {
 				answer = -1;
 			}
