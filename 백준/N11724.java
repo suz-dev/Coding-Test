e9e2773f5386e765cd309e30d6a554e9c2d823df
Sujeong Jo <bestsjj@naver.com>
2022-02-24 23:47:33 +0900

    DFS

---
+++ 백준/N11724.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+/*
+ * dfs
+ */
+public class N11724 {
+
+	static int N; // 정점의 개수
+	static int M; // 간선의 개수
+	static int[][] adj; // 인접 행렬
+
+	static boolean[] visit; // 방문 체크
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		N = sc.nextInt();
+		M = sc.nextInt();
+
+		adj = new int[N + 1][N + 1];
+
+		// 인접행렬 입력
+		for (int i = 0; i < M; i++) {
+			int u = sc.nextInt(); // 시작점
+			int v = sc.nextInt(); // 끝점
+
+			// 무향간선이기 때문에 양방향 연결
+			adj[u][v] = 1;
+			adj[v][u] = 1;
+		}
+
+		visit = new boolean[N + 1];
+
+		int cnt = 0; // 연결요소 개수
+
+		// 방문 배열을 돌면서 방문하지 않은 정점에 대해 탐색 후 연결요소 개수1 증가
+		for (int i = 1; i < N + 1; i++) {
+			if (!visit[i]) {
+				dfs(i);
+				cnt++;
+			}
+		}
+
+		System.out.println(cnt);
+
+	}
+
+	public static void dfs(int start) {
+
+		// 방문하였을 경우 true
+		visit[start] = true;
+
+		// 방문 배열에서 간선이 연결되어 있고, 방문을 하지 않았을 경우 dfs 탐색
+		for (int i = 1; i < N; i++) {
+			if (adj[start][i] == 1 && !visit[i]) {
+				dfs(i);
+			}
+		}
+	}
+
+}
