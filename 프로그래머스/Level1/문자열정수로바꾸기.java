ca6ffd046080d70f050e43cf26cd24e5d78cf3c4
Sujeong Jo <bestsjj@naver.com>
2022-04-11 18:45:11 +0900

    Level 1

---
+++ 프로그래머스/Level1/문자열정수로바꾸기.java
@@ -0,0 +1,9 @@
+package Level1;
+
+public class 문자열정수로바꾸기 {
+	public int solution(String s) {
+		int answer = 0;
+		answer = Integer.parseInt(s);
+		return answer;
+	}
+}
---
+++ 프로그래머스/Level1/수박수박수.java
@@ -0,0 +1,19 @@
+package Level1;
+
+public class 수박수박수 {
+	public String solution(int n) {
+		String answer = "";
+
+		for (int i = 0; i < n; i++) {
+
+			if (i % 2 != 0) {
+				answer += "박";
+			} else {
+				answer += "수";
+			}
+
+		}
+
+		return answer;
+	}
+}
