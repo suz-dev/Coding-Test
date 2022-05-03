0258f23b93eaacca9906d4731c7bb5751fa6f51e
Sujeong Jo <bestsjj@naver.com>
2022-05-03 19:17:40 +0900

    Arrays.sort

---
+++ 프로그래머스/Level2/전화번호목록.java
@@ -0,0 +1,19 @@
+package Level2;
+
+import java.util.Arrays;
+
+public class 전화번호목록 {
+	public boolean solution(String[] phone_book) {
+		boolean answer = true;
+
+		Arrays.sort(phone_book);
+
+		for (int i = 0; i < phone_book.length - 1; i++) {
+			if (phone_book[i + 1].contains(phone_book[i])) {
+				answer = false;
+			}
+		}
+
+		return answer;
+	}
+}
