344ede1b6bb8425b42bc07e9c7a9dd1ee8e87a15
Sujeong Jo <bestsjj@naver.com>
2022-05-24 17:24:12 +0900

    replaceAll()

---
+++ 프로그래머스/카카오/숫자문자열과영단어.java
@@ -0,0 +1,15 @@
+package Kakao;
+
+public class 숫자문자열과영단어 {
+	public static int solution(String s) {
+
+		String[] strArr = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+		for (int i = 0; i < strArr.length; i++) {
+			s = s.replaceAll(strArr[i], Integer.toString(i));
+		}
+
+		return Integer.parseInt(s);
+	}
+
+}
