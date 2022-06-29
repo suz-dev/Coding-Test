a6c10a09becc1c1eeef6c2cd1ec84df62ee27e23
Sujeong Jo <bestsjj@naver.com>
2022-06-29 19:30:24 +0900

    일부만 통과

--- 프로그래머스/카카오/주차요금계산.java
+++ 프로그래머스/카카오/주차요금계산.java
@@ -1,56 +1,60 @@
 package Kakao;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Map;
 
-// re
+// 테케3 통과
 public class 주차요금계산 {
 
-	public static HashMap<String, String> hashmap = new HashMap<String, String>();
-	public static ArrayList<Integer> fee = new ArrayList<Integer>();
+	public int[] solution(int[] fees, String[] records) {
+		int lastTime = getMin("23:59");
 
-	public ArrayList<Integer> solution(int[] fees, String[] records) {
+		Map<String, Integer> parking = new HashMap<>();
+		Map<String, Integer> times = new HashMap<>();
+		ArrayList<String> cars = new ArrayList<String>();
 
-		String[] record = new String[3];
-		for (int i = 0; i < records.length; i++) {
+		for (String record : records) {
 
-			record = records[i].split(" "); // 시각, 차량번호, 내역(입출차)
+			String[] rc = record.split(" "); // 시각, 차량번호, 내역(입출차)
+			int time = getMin(rc[0]);
+			String car = rc[1];
 
-			if (!hashmap.containsKey(record[1])) { // 입차
-				hashmap.put(record[1], record[0]); // 차량번호, 시각
-			} else { // 출차
-				parking(record[1], fees);
+			// 새로운 차 목록
+			if (!cars.contains(car)) {
+				cars.add(car);
+				times.put(car, 0);
+			}
+
+			if (parking.containsKey(car)) { // 출차
+				times.put(car, times.get(car) + (time - parking.get(car)));
+				parking.remove(car);
+			} else { // 입차
+				parking.put(car, time);
 			}
 		}
 
-		if (!hashmap.isEmpty()) { // 입차만 있는 경우
-			for (int i = 0; i < fee.size(); i++) {
-				parking("23:59", fees);
-			}
+		int[] ret = new int[cars.size()];
+		Collections.sort(cars);
+
+		for (int i = 0; i < cars.size(); i++) {
+			ret[i] = fees[1];
+			String car = cars.get(i);
+
+			int time = times.get(car) - fees[0];
+
+			if (parking.containsKey(car))
+				time += (lastTime - parking.get(car));
+			if (time > 0)
+				ret[i] += (Math.ceil(time / fees[2] * 1.0)) * fees[3];
 		}
-		return fee;
+
+		return ret;
 	}
 
-	public static void parking(String record, int[] fees) {
-
-		String[] in = hashmap.get(hashmap.remove(record)).split(":"); // 입차시간(쓴거는 지우기)
-		String[] out = record.split(":"); // 출차 시간:분
-
-		int time = Integer.parseInt(out[0]) - Integer.parseInt(in[0]);
-		int minute = Integer.parseInt(out[1]) - Integer.parseInt(in[1]);
-
-		int totalTime = time * 60 + minute;
-		int addTime = totalTime - fees[0];
-
-		if (totalTime <= fees[0]) {
-			fee.add(fees[1]);
-		} else {
-			if (addTime % fees[3] == 0) {
-				fee.add(fees[1] + (addTime / fees[3]) * fees[4]);
-			} else {
-				fee.add(fees[1] + (addTime / fees[3] + 1) * fees[4]);
-			}
-		}
-
+	public int getMin(String time) {
+		String[] t = time.split(":");
+		return Integer.valueOf(t[0]) * 60 + Integer.valueOf(t[1]);
 	}
 }
