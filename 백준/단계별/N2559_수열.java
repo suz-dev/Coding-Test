8201262fd4129daec8f35cd9e2e51860b44dd699
조수정 <suz.dev33@gmail.com>
2022-08-06 16:28:03 +0900

    범위설정 주의

---
+++ 백준/단계별/N2559_수열.java
@@ -0,0 +1,27 @@
+package 단계별;
+
+import java.util.Scanner;
+
+public class N2559_수열 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+		int K = sc.nextInt();
+
+		int[] arr = new int[N + 1];
+		for (int i = 1; i <= N; i++) {
+			arr[i] = arr[i - 1] + sc.nextInt();
+		}
+
+		int sum = -10000001;
+		int max = sum;
+
+		for (int i = N; i >= K; i--) {
+			sum = arr[i] - arr[i - K];
+			max = Math.max(max, sum);
+		}
+
+		System.out.println(max);
+	}
+}
