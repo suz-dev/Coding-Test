83b8a7107c99f98d63440510c7029307a1f62b91
Sujeong Jo <suz.dev33@gmail.com>
2023-08-22 22:29:30 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 9.43 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.java
@@ -0,0 +1,53 @@
+public class Solution {
+
+	public int[] solution(int rows, int columns, int[][] queries) {
+		int[] answer = new int[queries.length];
+
+		int[][] nums = new int[rows][columns];
+		int num = 1;
+
+		// 행렬 rows x columns 채우기
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < columns; j++) {
+				nums[i][j] = num++;
+			}
+		}
+
+		for (int i = 0; i < queries.length; i++) {
+
+			int[] query = queries[i];
+			int r1 = query[0] - 1, c1 = query[1] - 1, r2 = query[2] - 1, c2 = query[3] - 1;
+			int tmp = nums[r1][c1]; // 시작값 (회전으로 덮어씌워질 값 미리 저장)
+			int min = tmp; // 최소값
+
+			// 왼쪽 테두리 회전
+			for (int r = r1 + 1; r <= r2; r++) {
+				min = Math.min(min, nums[r][c1]);
+				nums[r - 1][c1] = nums[r][c1];
+			}
+
+			// 아래 테두리 회전
+			for (int c = c1 + 1; c <= c2; c++) {
+				min = Math.min(min, nums[r2][c]);
+				nums[r2][c - 1] = nums[r2][c];
+			}
+
+			// 오른쪽 테두리 회전
+			for (int r = r2 - 1; r >= r1; r--) {
+				min = Math.min(min, nums[r][c2]);
+				nums[r + 1][c2] = nums[r][c2];
+			}
+			// 윗 테두리 회전
+			for (int c = c2 - 1; c > c1; c--) {
+				min = Math.min(min, nums[r1][c]);
+				nums[r1][c + 1] = nums[r1][c];
+			}
+
+			nums[r1][c1 + 1] = tmp;
+			answer[i] = min;
+		}
+
+		return answer;
+	}
+
+}
