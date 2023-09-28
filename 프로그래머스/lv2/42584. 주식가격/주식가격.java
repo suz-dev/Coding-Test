bdc8341da0ba3f4811beaca432d97819d428b4b7
Sujeong Jo <suz.dev33@gmail.com>
2023-09-28 13:05:01 +0900

    [level 2] Title: 주식가격, Time: 14.63 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+
+        for(int i = 0; i < prices.length; ++i){
+            for(int j = i+1; j < prices.length; ++j){
+                
+                if(prices[i] <= prices[j])
+                    answer[i]++;
+                else{
+                    answer[i]++;
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
