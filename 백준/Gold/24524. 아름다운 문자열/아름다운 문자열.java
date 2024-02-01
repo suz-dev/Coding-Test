f1def58ac7ee31d5e4f2689549eafae0c12aa684
Sujeong Jo <suz.dev33@gmail.com>
2024-02-01 17:22:50 +0900

    [Gold V] Title: 아름다운 문자열, Time: 312 ms, Memory: 21516 KB -BaekjoonHub

--- 백준/Gold/24524. 아름다운 문자열/아름다운 문자열.java
+++ 백준/Gold/24524. 아름다운 문자열/아름다운 문자열.java
@@ -2,51 +2,31 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static String S, T;
-    static Map<Character, Queue<Integer>> map;
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        S = br.readLine();
-        T = br.readLine();
+        String S = br.readLine();
+        String T = br.readLine();
 
-        map = new HashMap<>();
-        for(int i = 0; i< S.length(); i++){
-            char ch = S.charAt(i);
+        int sLen = S.length();
+        int tLen = T.length();
 
-            Queue<Integer> tmp = map.getOrDefault(ch, new LinkedList<>());
-            tmp.add(i);
-            map.put(ch, tmp);
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < tLen; i++) map.put(T.charAt(i), i + 1);
+
+        int[] arr = new int[tLen + 1];
+        arr[0] = sLen;
+
+        for (int i = 0; i < sLen; i++) {
+            if(!map.containsKey(S.charAt(i))) continue;
+
+            int t = map.get(S.charAt(i));
+            if (arr[t - 1] == 0) continue;
+
+            arr[t - 1]--;
+            arr[t]++;
         }
 
-        int cnt = 0;
-        out: while(true){
-            int preIdx = -1;
-
-            for(int i = 0; i < T.length(); i++){
-                char key = T.charAt(i);
-                Queue<Integer> val = map.getOrDefault(key, new LinkedList<>());
-
-                if(val.isEmpty()) {
-                    break out;
-                }
-
-                int nowIdx = val.poll();
-
-                while(nowIdx < preIdx ){
-                    if(val.isEmpty()){
-                        break out;
-                    }
-                    nowIdx = val.poll();
-                }
-
-                preIdx = nowIdx;
-                map.put(key, val);
-            }
-            cnt++;
-        }
-
-        System.out.println(cnt);
+        System.out.println(arr[tLen]);
     }
 }
