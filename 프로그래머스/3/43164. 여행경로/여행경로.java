147a8141f720294baed290eb37eae030d6ba60aa
Sujeong Jo <suz.dev33@gmail.com>
2024-05-01 17:55:34 +0900

    [level 3] Title: 여행경로, Time: 8.26 ms, Memory: 80.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    
+    static boolean[] visited;
+    static int N;
+    static PriorityQueue<String> pq = new PriorityQueue<>();
+    
+    public static String[] solution(String[][] tickets) {
+        String[] answer = {};
+        
+        N = tickets.length;
+        visited = new boolean[N];
+        
+        dfs(tickets, "ICN", 0, "ICN");
+        answer = pq.poll().split("-");
+        
+        return answer;
+    }
+    
+    public static void dfs(String[][] tickets, String departure, int cnt, String path){
+        if(cnt == N){
+            pq.add(path);
+            return;
+        }
+        
+        for(int i = 0;i < N; i++){
+            if(!visited[i] && departure.equals(tickets[i][0])) {
+                visited[i] = true;
+                dfs(tickets, tickets[i][1], cnt+1, path+"-"+tickets[i][1]);
+                visited[i] = false;
+            }
+        }
+    }
+}
